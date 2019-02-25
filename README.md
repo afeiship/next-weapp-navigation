871bd9780105f4f07554f6f092a37386a96a6c5d
afeiship <1290657123@qq.com>
2019-02-25 10:28:37 +0800

    feat: refactor

--- README.md
+++ README.md
@@ -7,15 +7,21 @@ npm install -S afeiship/next-weapp-navigation --registry=https://registry.npm.ta
 ```
 
 ## apis:
-| api | params | description   |
-|-----|--------|---------------|
-| get | -      | desc balabala |
+| api     | params              | description                        |
+|---------|---------------------|------------------------------------|
+| loading | (inValue,inOptions) | 在当前页面显示、隐藏导航条加载动画 |
+| title   | (inOptions)         | 动态设置当前页面的标题|g           |
+| color   | (inOptions)         | 设置页面导航条颜色                 |
 
 ## usage:
 ```js
 import NxWeappNavigation from 'next-weapp-navigation';
 
 // code goes here:
+NxWeappNavigation.loading(true)
+NxWeappNavigation.loading(false)
+NxWeappNavigation.title({ title:'test' })
+NxWeappNavigation.frontColor({ color:'test' })
 ```
 
 ## resources:
--- src/next-weapp-navigation.js
+++ src/next-weapp-navigation.js
@@ -1,6 +1,7 @@
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var nxCapitalize = nx.capitalize || require('next-capitalize');
   var nxWxPromisify = nx.wxPromisify || require('next-wx-promisify');
   var wx = global.wx;
 
@@ -12,15 +13,14 @@
           wx[action](nx.mix(nxWxPromisify(resolve, reject), inOptions));
         });
       },
-      title: function(inOptions) {
-        return new Promise(function(resolve, reject) {
-          wx.setNavigationBarTitle(nx.mix(nxWxPromisify(resolve, reject), inOptions));
-        });
-      },
-      color: function(inOptions) {
-        return new Promise(function(resolve, reject) {
-          wx.setNavigationBarColor(nx.mix(nxWxPromisify(resolve, reject), inOptions));
-        });
+      'title,color': function(inName) {
+        return function(inOptions) {
+          return new Promise(function(resolve, reject) {
+            wx['setNavigationBar' + nxCapitalize(inName)](
+              nx.mix(nxWxPromisify(resolve, reject), inOptions)
+            );
+          });
+        };
       }
     }
   });
